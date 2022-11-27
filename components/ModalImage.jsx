091f823ab57f99282ae0feb748fd7bbf1520b237
hala <halasliimen@gmail.com>
2022-11-27 14:06:59 +0100

    Update ModalImage.jsx

--- components/ModalImage.jsx
+++ components/ModalImage.jsx
@@ -2,11 +2,19 @@ import React from "react";
 
 //styles
 import styles from "@sa/styles/components/ModalImage.module.scss";
+import Image from "next/image";
 
 function ModalImage({ image, name, onClick }) {
   return (
     <div id={styles.ModalImage} onClick={onClick}>
-      <img src={image} alt={name} />
+      <Image
+        src={image}
+        alt={name}
+        width="0"
+        height="0"
+        sizes="100vw"
+        style={{ height: "80%", width: "auto" }}
+      />
     </div>
   );
 }
