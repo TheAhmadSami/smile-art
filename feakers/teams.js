90d1601673109f3b7029a82d1a3a0d2bd247039d
hala <halasliimen@gmail.com>
2022-11-27 16:57:38 +0100

    add data

---
+++ feakers/teams.js
@@ -0,0 +1,54 @@
+import assets from "@sa/assets";
+
+const data = [
+  {
+    id: 1,
+    title: "أحمد مجدي",
+    image: assets.teamOne.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 2,
+    title: "أحمد مجدي",
+    image: assets.teamTwo.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 3,
+    title: "أحمد مجدي",
+    image: assets.teamOne.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 4,
+    title: "أحمد مجدي",
+    image: assets.teamTwo.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 5,
+    title: "أحمد مجدي",
+    image: assets.teamOne.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 6,
+    title: "أحمد مجدي",
+    image: assets.teamTwo.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 7,
+    title: "أحمد مجدي",
+    image: assets.teamOne.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+  {
+    id: 8,
+    title: "أحمد مجدي",
+    image: assets.teamTwo.src,
+    description: "الزمالة البريطانية في طب الأسنان من جامعة لندن",
+  },
+];
+
+export default data;
