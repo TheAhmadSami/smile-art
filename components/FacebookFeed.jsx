2c0b18e8d118e5b14d68ef6fce410ffb9905d43b
hala <halasliimen@gmail.com>
2022-11-27 16:56:15 +0100

    Update FacebookFeed.jsx

--- components/FacebookFeed.jsx
+++ components/FacebookFeed.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
+
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+
+
 //styles
 import styles from "@sa/styles/components/FacebookFeed.module.scss";
 
 const FacebookFeed = () => {
+  const { t } = useTranslation();
   return (
     <div id={styles.facebookFeed}>
       <div className={styles.row}>
         <div className={styles.row2}>
           <i className="fab fa-facebook"></i>
-          <h1 className={styles.title}>فيسبوك فييد</h1>
+          <h1 className={styles.title}>{t("facebookfeed")}</h1>
         </div>
-        <h6 className={styles.subtitle}>عرض المزيد</h6>
+        <h6 className={styles.subtitle}>{t("view_more")}</h6>
       </div>
     </div>
   );
