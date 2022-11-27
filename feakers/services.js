32147e20b004541e7311c4cedece58da37fb0404
hala <halasliimen@gmail.com>
2022-11-27 17:00:46 +0100

    Update services.js

--- feakers/services.js
+++ feakers/services.js
@@ -2,44 +2,59 @@ import assets from "@sa/assets";
 const data = [
   {
     id: 1,
-    Image: assets.gallery1.src,
-    name: "1",
+    title: "زراعة الأسنان",
+    image: assets.serviceOne.src,
+    description:
+      "يتم وضع الزرعات في عظام الفك الضعيفة ( قليلة الكثافة أو الحجم ) ثم الإنتظار 3 أشهر على الأقل حتى تتم عملية الإلتآم مع الزرعة و يتم بعد ذلك وضع جسور الأسنان المراد تركيبها",
   },
   {
     id: 2,
-    Image: assets.gallery2.src,
-    name: "si",
+    title: "CAD/CAM",
+    image: assets.serviceTwo.src,
+    description:
+      "يوفر المركز تكنولوجيا و خدمات الكاد كام في مجال تركيب التيجان و الجسور(الإي ماكس و الزيركون) و كذلك الخدمات التجميلية للأسنان في زيارة واحدة فقط عن طريق أخذ المقاسات و تصوير الفم بالكمبيوتر ",
   },
   {
     id: 3,
-    Image: assets.gellery3.src,
-    name: "sqdqhi",
+    title: "Digital Smile Design",
+    image: assets.serviceThree.src,
+    description:
+      "يمكنك الأن الحصول على الإبتسامة التي تتمناها و تختارها بنفسك . خطوات بسيطة على الكمبيوتر لتكون إبتسامتك جاهزة أمام عينيك قبل صنعها        ",
   },
   {
     id: 4,
-    Image: assets.gallery1.src,
-    name: "jjk",
+    title: "Hollywood Smile إبتسامة المشاهير",
+    image: assets.serviceFour.src,
+    description:
+      "إحصل على إبتسامة الفنانين و المشاهير كما تتمناها بالشكل و درجة اللون البيضاء التي تختارها .        ",
   },
-
   {
     id: 5,
-    Image: assets.gallery1.src,
-    name: "1",
+    title: "زراعة الأسنان",
+    image: assets.serviceOne.src,
+    description:
+      "يتم وضع الزرعات في عظام الفك الضعيفة ( قليلة الكثافة أو الحجم ) ثم الإنتظار 3 أشهر على الأقل حتى تتم عملية الإلتآم مع الزرعة و يتم بعد ذلك وضع جسور الأسنان المراد تركيبها",
   },
   {
     id: 6,
-    Image: assets.gallery2.src,
-    name: "si",
+    title: "CAD/CAM",
+    image: assets.serviceTwo.src,
+    description:
+      "يوفر المركز تكنولوجيا و خدمات الكاد كام في مجال تركيب التيجان و الجسور(الإي ماكس و الزيركون) و كذلك الخدمات التجميلية للأسنان في زيارة واحدة فقط عن طريق أخذ المقاسات و تصوير الفم بالكمبيوتر ",
   },
   {
     id: 7,
-    Image: assets.gellery3.src,
-    name: "sqdqhi",
+    title: "Digital Smile Design",
+    image: assets.serviceThree.src,
+    description:
+      "يمكنك الأن الحصول على الإبتسامة التي تتمناها و تختارها بنفسك . خطوات بسيطة على الكمبيوتر لتكون إبتسامتك جاهزة أمام عينيك قبل صنعها        ",
   },
   {
     id: 8,
-    Image: assets.gallery1.src,
-    name: "jjk",
+    title: "Hollywood Smile إبتسامة المشاهير",
+    image: assets.serviceFour.src,
+    description:
+      "إحصل على إبتسامة الفنانين و المشاهير كما تتمناها بالشكل و درجة اللون البيضاء التي تختارها .        ",
   },
 ];
 
